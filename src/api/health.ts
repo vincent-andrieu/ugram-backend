a3860e97282291ba7bd8d7888e8ec47b5b972b96
Vincent Andrieu <vincent.andrieu@epitech.eu>
2023-01-23 23:31:28 -0500

    Edit health routes with TemplateRoutes

--- src/api/health.ts
+++ src/api/health.ts
@@ -1,21 +1,25 @@
 import { Express } from "express";
 
 import { RouteWhitelister } from "@middlewares/authentification";
+import TemplateRoutes from "./templateRoutes";
 
-export default class HealthRoutes {
+export default class HealthRoutes extends TemplateRoutes {
 
-    constructor(private _app: Express, routeWhitelister: RouteWhitelister) {
+    constructor(app: Express, routeWhitelister: RouteWhitelister) {
+        super(app);
+
         this._init();
 
+        // / is already whitelisted by default
         routeWhitelister("/health");
     }
 
     private _init() {
-        this._app.get("/", (_, res) => {
+        this._route<never, string>("get", "/", (_, res) => {
             res.status(200).send("OK");
         });
 
-        this._app.get("/health", (_, res) => {
+        this._route<never, never>("get", "/health", (_, res) => {
             res.sendStatus(200);
         });
     }
